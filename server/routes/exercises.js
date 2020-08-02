5d4aff90458c33c9a2001203d33b55d507b7b7f2
JalanshMunshi <jalansh.munshi1366@gmail.com>
2020-08-02 21:58:27 +0530

    Added API endpoints for delete, update and getting user by id. Added comments.

--- server/routes/exercises.js
+++ server/routes/exercises.js
@@ -1,12 +1,14 @@
 const router = require('express').Router();
 const Exercise = require('../models/exercise.model');
 
+// Get the list of exercises from the DB
 router.route('/').get((req, res) => {
     Exercise.find()
     .then(exercises => res.json(exercises))
     .catch(err => res.status(400).json(`Error: ${err}.`));
 });
 
+// Add a new exercise
 router.route('/add').post((req, res) => {
     const username = req.body.username;
     const description = req.body.description;
@@ -24,5 +26,35 @@ router.route('/add').post((req, res) => {
     .then(() => res.json('Exercise added!'))
     .catch(err => res.status(400).json(`Error: ${err}.`));
 });
+
+// Get an exercise by its id
+router.route('/:id').get((req, res) => {
+    Exercise.findById(req.params.id)
+    .then(exercise => res.json(exercise))
+    .catch(err => res.status(400).json(`Error: ${err}.`));
+});
+
+// Deletes an exercise by its id
+router.route('/:id').delete((req, res) => {
+    Exercise.findByIdAndDelete(req.params.id)
+    .then(() => res.json('Exercise deleted.'))
+    .catch(err => res.status(400).json(`Error: ${err}.`));
+})
+
+// Updates exercise details
+router.route('/update/:id').post((req, res) => {
+    Exercise.findById(req.params.id)
+    .then(exercise => {
+        exercise.username = req.body.username;
+        exercise.description = req.body.description;
+        exercise.duration = Number(req.body.duration);
+        exercise.date = Date.parse(req.body.date);
+
+        exercise.save()
+        .then(() => res.json('Exercise updated.'))
+        .catch(err => res.status(400).json(`Error: ${err}.`));
+    })
+    .catch(err => res.status(400).json(`Error: ${err}.`));
+})
 
 module.exports = router;
