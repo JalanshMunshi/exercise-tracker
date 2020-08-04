a7e48ea560e3056277484875714b86abbfabdd36
JalanshMunshi <jalansh.munshi1366@gmail.com>
2020-08-04 20:22:42 +0530

    Added the required components. Implemented create exercise component.

---
+++ src/components/edit-exercise.component.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Component } from "react";
+
+class EditExercise extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Edit exercise page.</h1>
+            </div>
+        )
+    }
+};
+
+export default EditExercise;
---
+++ src/components/exercises-list.component.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Component } from "react";
+
+class ExercisesList extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Exercises list page.</h1>
+            </div>
+        )
+    }
+};
+
+export default ExercisesList;
